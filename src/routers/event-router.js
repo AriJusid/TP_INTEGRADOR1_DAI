8f8a34664ddf6e3f64f7af466dd7466daf2cc05f
Android Studio <devandroid@ort>
2025-07-08 16:01:13 -0300

    Avanze search by ID

--- src/routers/event-router.js
+++ src/routers/event-router.js
@@ -21,6 +21,8 @@ router.get('/', async (req, res) => {
     }
 });
 
+//FALTA ARQUITECTURA
+
 router.get('/search', async (req, res) => {
     const { name, start_date, tag } = req.query;
 
@@ -59,6 +61,60 @@ router.get('/search', async (req, res) => {
     }
   });
   
-  
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    
+    try{
+        
+        console.log("Error de conexión")
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+
+        const sql =  `
+        SELECT
+        e.id,
+        e.name,
+        e.description,
+        e.start_date,
+        e.duration_in_minutes,
+        e.price,
+        e.enabled_for_enrollment,
+        el.max_capacity,
+        json_build_object(
+            'id', u.id,
+            'first_name', u.first_name,
+            'last_name', u.last_name,
+            'username', u.username
+        ),
+        json_build_object(
+            'id', el.id,
+            'name', el.name,
+            'full_address', el.full_address,
+            'latitude', el.latitude,
+            'longitude', el.longitude,
+            'max_capacity', el.max_capacity
+        )
+    FROM
+        events e
+    JOIN event_locations el ON e.id_event_location = el.id
+    JOIN users u ON e.id_creator_user = u.id;
+
+    WHERE id= $1`
+        const values = [id]
+        let result = await pool.query(sql, values);    
+
+        res.status(StatusCodes.OK).send(result.rows[0]);
+    }
+    catch(error){
+        if(isNaN(id)){
+            res.status(StatusCodes.BAD_REQUEST).send("ID inválido");
+        }
+        else if (!eventosArray.contains(id)){
+            res.status(StatusCodes.NOT_FOUND).send("Evento inexsitente");
+        }
+
+        console.log(error)
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
+    }
+})
 
 export default router;
