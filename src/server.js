581e5802ca0dde99a717f65f72335c7c797dde8e
Android Studio <devandroid@ort>
2025-08-04 12:15:13 -0300

    feat: details

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import LocationsRouter from './routers/locations-router.js'
 const app  = express();
 const port = 4000;
 
-const allowedOrigins = ['http://localhost:5173', 'https://8ab3039b96f6.ngrok-free.app'];
+const allowedOrigins = ['http://localhost:5173', 'https://29f049d92c52.ngrok-free.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
